b4e5ce4dc2a0cce74158561bde5d352f7ac98d36
MolecularCat <geb@inbox.ru>
2023-02-16 12:05:06 +0300

    add news class. add console writeline logic

--- Polymorphism_example/Program.cs
+++ Polymorphism_example/Program.cs
@@ -4,9 +4,32 @@ namespace Polymorphism_example
 {
     class Program
     {
+        public class News
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public DateTime Created { get; set; } = DateTime.Now;
+            public string GetHappyNwes()
+            {
+                string happyNews = "Happy! " + Description;
+
+                return happyNews;
+            }
+        }
+
         static void Main(string[] args)
         {
+            News newsObject = new News();
+
+            newsObject.Description = "First News";
+
+
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine(newsObject.Description);
+
+            Console.WriteLine(newsObject.GetHappyNwes());
         }
     }
 }
