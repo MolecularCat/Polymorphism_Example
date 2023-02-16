7e316d7273bd19532628d04d9f1766ed39e2cbee
MolecularCat <geb@inbox.ru>
2023-02-16 11:11:49 +0300

    add new project

---
+++ Polymorphism_example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Polymorphism_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
